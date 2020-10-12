b30745975f4afe431ed58f116c30afc040f45fb8
Amsal Khan <amsalkhan@iul.ac.in>
2020-10-12 12:45:29 +0530

    master

--- scriptallweek-cse2-2022.bat
+++ scriptallweek-cse2-2022.bat
@@ -2,7 +2,7 @@
 rem DOW is variable used for the day of the week. 0 stands for Sunday and 6 stands for Saturday
 rem min stands for minutes and hour stands for hour number.
 rem This is specifically made for CSE-2 2022 Batch Opensource & SPQM opted students. For Java opted students, find and replace CS309_A with your java subject code.
-rem CS301_B doesn't show in my left navigation panel so it can't be automated for now :(
+rem Courses which doesn't show in your left navigation bar, you need to use its course link directly as a argument instead of subject code to get its attendance done! :D
 rem "rem" keyword is used for making the line comment. Some of the lines are commented which you can uncomment for your java subject. For opensource students, its already done :)
 rem Make sure to keep AACS_ILI.exe in the same folder
 rem Set your enrollment number
@@ -18,13 +18,13 @@ goto :comehere
 :comehere
 
 rem Monday Schedule
-rem if %hour% EQU 9 (
-rem	if %min% EQU 01	(
-rem		if %DOW% EQU 1 (
-rem		start "" /W "AACS_ILI.exe" %enroll% %pass% CS301_B
-rem		)
-rem	)
-rem )
+ if %hour% EQU 9 (
+	if %min% EQU 01	(
+		if %DOW% EQU 1 (
+		start "" /W "AACS_ILI.exe" %enroll% %pass% https://ilizone.iul.ac.in/course/view.php?id=3863
+		)
+	)
+ )
 
 if %hour% EQU 10 (
 	if %min% EQU 31	(
@@ -93,13 +93,13 @@ if %hour% EQU 16 (
 )
 
 rem Wednesday Schedule
-rem if %hour% EQU 9 (
-rem 	if %min% EQU 1	(
-rem 		if %DOW% EQU 3 (
-rem 		start "" /W "C:\Users\Administrator\Desktop\script\AACS_ILI.exe" %enroll% %pass% CS301_B
-rem 		)
-rem 	)
-rem )
+ if %hour% EQU 9 (
+ 	if %min% EQU 1	(
+ 		if %DOW% EQU 3 (
+ 		start "" /W "C:\Users\Administrator\Desktop\script\AACS_ILI.exe" %enroll% %pass% https://ilizone.iul.ac.in/course/view.php?id=3863
+ 		)
+ 	)
+ )
 
 if %hour% EQU 10 (
 	if %min% EQU 31	(
@@ -159,13 +159,13 @@ if %hour% EQU 14 (
 )
 
 rem Friday Schedule
-rem if %hour% EQU 9 (
-rem 	if %min% EQU 1	(
-rem 		if %DOW% EQU 5 (
-rem 		start "" /W "C:\Users\Administrator\Desktop\script\AACS_ILI.exe" %enroll% %pass% CS301_B
-rem 		)
-rem 	)
-rem )
+ if %hour% EQU 9 (
+ 	if %min% EQU 1	(
+ 		if %DOW% EQU 5 (
+ 		start "" /W "C:\Users\Administrator\Desktop\script\AACS_ILI.exe" %enroll% %pass% https://ilizone.iul.ac.in/course/view.php?id=3863
+ 		)
+ 	)
+ )
 
 if %hour% EQU 10 (
 	if %min% EQU 31	(
